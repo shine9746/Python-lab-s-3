622621b050e724a7a1bc18dbeee632178d42dc4e
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-13 12:12:47 +0530

    Update Python-lab-s3-1.py

--- python-lab-s3-1/Python-lab-s3-1.py
+++ python-lab-s3-1/Python-lab-s3-1.py
@@ -1,2 +1,3 @@
-with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/python-lab-s3-1/Python-lab-s3-1.txt","r") as f:
-    print(f.read())
+#programme to read an entire text file
+with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/python-lab-s3-1/Python-lab-s3-1.txt","r") as textFile:
+    print(textFile.read())
