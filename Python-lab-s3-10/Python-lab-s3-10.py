c394fa68da065dcfe8a2d8762ee942b8fa30e258
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-19 12:54:04 +0530

    Add files via upload

---
+++ Python-lab-s3-10/Python-lab-s3-10.py
@@ -0,0 +1,23 @@
+#Python programme to count the words frequency on a text file
+with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/Python-lab-s3-10/Python-lab-s3-10.txt", 'r') as File:
+
+ a = File.read().strip()  #read and stripped unwanted nlines
+
+
+b = {}   
+
+for i in a:
+	
+	words = a.casefold().split(" ")  #splited into word and caseinsensitive
+		
+	
+for x in words:
+	if x in b:
+		b[x] += 1
+	else:
+		b[x] = 1
+print("Repeating words of text file is:")
+for x in b:
+	print(x,":",b[x])
+
+
