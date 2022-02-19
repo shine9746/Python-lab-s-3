ca114017fa0164195c89b54a11548ebf73b26745
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-19 20:51:51 +0530

    Add files via upload

---
+++ Python-lab-s3-12/Python-lab-s3-12.py
@@ -0,0 +1,14 @@
+#Python programme to write  list into text file
+with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/Python-lab-s3-12/Python-lab-s3-12.txt","a") as File:
+
+ List = File.write("apple,mango,pineapple,guava,plum,cherry")
+with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/Python-lab-s3-12/Python-lab-s3-12.txt","r") as File:
+
+ List = File.read().split(" ")
+
+ x = list(List)
+ print("List = " ,x)
+ print(type(x))
+
+
+
