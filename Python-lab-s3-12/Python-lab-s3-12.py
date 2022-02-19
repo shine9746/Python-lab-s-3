1ac7789cca7842546f57588a7ab6e2852992f3c8
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-19 21:03:23 +0530

    Update Python-lab-s3-12.py

--- Python-lab-s3-12/Python-lab-s3-12.py
+++ Python-lab-s3-12/Python-lab-s3-12.py
@@ -1,5 +1,5 @@
 #Python programme to write a list to text file
-with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/Python-lab-s3-12/Python-lab-s3-12.txt","a") as File:
+with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/Python-lab-s3-12/Python-lab-s3-12.txt","w") as File:
 
  List = File.write("apple,mango,pineapple,guava,plum,cherry")
 with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/Python-lab-s3-12/Python-lab-s3-12.txt","r") as File:
