7587cdce81d522c461720746476dc52c6d78d3c3
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-19 11:38:24 +0530

    Add files via upload

---
+++ Python-lab-s3-9/Python-lab-s3-9.py
@@ -0,0 +1,10 @@
+with open("C:/Users/USER/Desktop/Labs/Python-lab/Python-lab-s-3/Python-lab-s3-9/Python-lab-s3-9.txt", 'r') as File:
+
+  count=0
+
+  file = File.read().split("\n")
+
+  for x in file:
+		 if x:
+		  count += 1
+print("The number of lines in text file is : ",count)
